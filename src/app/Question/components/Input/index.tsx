e9056612e25770f16f9e9062ba8c4d73ebf8a407
Бойко Артем <boiko.a@MacBook-Pro-Bojko.local>
2023-08-28 00:07:02 +0300

    input & textarea

--- src/app/Question/components/Input/index.tsx
+++ src/app/Question/components/Input/index.tsx
@@ -1,13 +1,28 @@
-import { FC } from 'react';
+import { FC, ChangeEvent } from 'react';
+import { observer } from 'mobx-react-lite';
 import { Input as AntdInput } from 'antd';
-import { _ } from 'utils';
+import { root } from 'store';
 import styles from './styles.module.css';
 
-export const Input: FC = () => {
+export const Input: FC = observer(() => {
+  const { currentQuestion, answers } = root.questions;
+
+  const change = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.value.trim().length) {
+      answers.storage.delete(currentQuestion.id);
+      return;
+    }
+    answers.storage.set(currentQuestion.id, e.target.value);
+  };
+
+  const value = answers.getTyped<'input'>(currentQuestion.id);
+  
   return (
     <AntdInput
+      value={value}
       className={styles.input}
       placeholder='Напишите ваш ответ'
+      onChange={change}
     />
   );
-};
+});
--- src/app/Question/components/Textarea/index.tsx
+++ src/app/Question/components/Textarea/index.tsx
@@ -1,16 +1,31 @@
-import { FC } from 'react';
+import { FC, ChangeEvent } from 'react';
+import { observer } from 'mobx-react-lite';
 import { Input as AntdInput } from 'antd';
-import { _ } from 'utils';
+import { root } from 'store';
 import styles from './styles.module.css';
 
-export const Textarea: FC = () => {
+export const Textarea: FC = observer(() => {
+  const { currentQuestion, answers } = root.questions;
+
+  const change = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    if (!e.target.value.trim().length) {
+      answers.storage.delete(currentQuestion.id);
+      return;
+    }
+    answers.storage.set(currentQuestion.id, e.target.value);
+  };
+
+  const value = answers.getTyped<'input'>(currentQuestion.id);
+
   return (
     <AntdInput.TextArea
       className={styles.textarea}
+      value={value}
       rows={4}
       placeholder='Напишите ваш ответ'
       maxLength={2000}
       size='large'
+      onChange={change}
     />
   );
-};
+});
